4666d7ba0f13250c8907c15a180bb763113cbb81
ebina <ebina.engr@gmail.com>
2023-09-29 21:12:57 +0900

    [update] export constants, helpers, types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-export { useMinesweeper, GAME_MODE_LIST } from './useMinesweeper';
+// main entry point of the library
 
+// TODO: Separate the main logic, helper functions, and type definitions from each files
+
+// hooks
+export { useMinesweeper } from './useMinesweeper';
+
+// constants
+export { GAME_MODE_LIST } from './useMinesweeper';
+
+// helpers
+export { isMine, isCount, isEmpty, isOpened, isUnopened, isFlagged } from './functions/board';
+
+// types
 export type { Minesweeper, GameMode } from './useMinesweeper';
+export type { Board, BoardConfig, CellData } from './functions/board';
